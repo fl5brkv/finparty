b67521bc4029a253a0397dc440f06cc65f94f74d
fl5brkv <fl5brkv@proton.me>
2025-04-03 17:18:25 +0200

    add github oauth endpoint + credentials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,10 @@ export default defineNuxtConfig({
         clientId: import.meta.env.NUXT_OAUTH_GOOGLE_CLIENT_ID,
         clientSecret: import.meta.env.NUXT_OAUTH_GOOGLE_CLIENT_SECRET,
       },
+      github: {
+        clientId: import.meta.env.NUXT_OAUTH_GITHUB_CLIENT_ID,
+        clientSecret: import.meta.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET,
+      },
     },
   },
 });
