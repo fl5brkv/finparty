5c13c63ba73593147f55cdd62133a414488dc1e2
fl5brkv <fl5brkv@proton.me>
2025-04-03 15:09:22 +0200

    add nuxthub/core, wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-
-  devtools: { enabled: false },
+  devtools: {enabled: false},
 
   future: {
     compatibilityVersion: 4,
   },
-})
+
+  modules: ['@nuxthub/core'],
+
+  hub: {
+    database: true,
+  },
+});
